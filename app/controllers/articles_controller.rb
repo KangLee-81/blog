35e49cebf3ce635369b1a2812dbb779f3760f8d8
terryzvk <657926338@qq.com>
2017-03-31 23:26:51 +0800

    添加一些代碼

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,17 @@ class ArticlesController < ApplicationController
 	      render 'new'
 	    end	
 	end
+    
+    def update
+    	@article = Article.find(params[:id])
+
+    	if @article.update(article.params)
+    		redirect_to @article
+    	else
+    		render 'edit'
+    	end	
+    end
+
 	private
 
 	def article_params
