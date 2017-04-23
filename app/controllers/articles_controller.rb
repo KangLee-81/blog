380ddd60fd581a67737e6a5c9e2426a110aa368c
tzhang <tzhang@xogrp.com>
2017-04-23 23:58:02 +0800

    增加了一些新文件

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+	
 	def create
 		@article = Article.new(article_params)
 
@@ -27,11 +28,19 @@ class ArticlesController < ApplicationController
     def update
     	@article = Article.find(params[:id])
 
-    	if @article.update(article.params)
+    	if @article.update(article_params)
     		redirect_to @article
     	else
     		render 'edit'
     	end	
+    end
+
+    def destroy
+    	@article = Article.find(params[:id])
+    	@article.destroy
+
+    	redirect_to articles_path
+    	
     end
 
 	private
